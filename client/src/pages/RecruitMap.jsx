09b821b9fb34c5939829d6d5e97b3ec21fe4997c
sojinjang <jsojin6119@gmail.com>
2022-12-16 15:13:12 +0900

    :lipstick: style(map): 지도로 보기에 필요한 컴포넌트 추가

--- client/src/pages/RecruitMap.jsx
+++ client/src/pages/RecruitMap.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import tw from 'tailwind-styled-components';
 
+const BackGround = tw.div`
+  w-screen h-screen flex justify-center items-center
+`;
+
 const RecruitMap = () => {
   return (
     <div>
+      <BackGround
+        style={{ backgroundImage: 'url(/images/bg1.png)' }}></BackGround>
       <LocationSelectionBar />
+      <MarkerDescription />
       <ViewSelectionBar />
+      <BanggaCharacter />
+      <MapContainer />
     </div>
   );
 };
 
 const LocationSelectionBar = () => {
-    <div>
-      <button>홍대</button>
-      <button>강남</button>
-      <button>건대</button>
-    </div>
-}
+  <div>
+    <button>홍대</button>
+    <button>강남</button>
+    <button>건대</button>
+  </div>;
+};
 
 const ViewSelectionBar = () => {
   <div>
@@ -28,8 +37,8 @@ const ViewSelectionBar = () => {
       <img></img>
       <div>Map</div>
     </div>
-  </div>
-}
+  </div>;
+};
 
 const MapContainer = () => {
   <div>
@@ -38,7 +47,26 @@ const MapContainer = () => {
       <div></div>
     </div>
     <div></div>
-  </div>
-}
+  </div>;
+};
+
+const BanggaCharacter = () => {
+  <div>
+    <img></img>
+  </div>;
+};
+
+const MarkerDescription = () => {
+  <div>
+    <div>
+      <img></img>
+      <div>모집중</div>
+    </div>
+    <div>
+      <img></img>
+      <div>모집완료</div>
+    </div>
+  </div>;
+};
 
 export default RecruitMap;
