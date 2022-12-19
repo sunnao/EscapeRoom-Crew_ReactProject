c417872e95bfcfc6ca45b617ce4df036b60885a2
sojinjang <jsojin6119@gmail.com>
2022-12-20 00:33:02 +0900

    :lipstick: style(map): 컴포넌트들 배치 및 스타일링

--- client/src/pages/RecruitMap.jsx
+++ client/src/pages/RecruitMap.jsx
@@ -1,57 +1,45 @@
 import React from 'react';
 import tw from 'tailwind-styled-components';
+import Background from '../components/common/Background';
 import RecruitTypeIcon from '../components/recruit/RecruitTypeIcon';
 import { RegionButton } from '../components/buttons/Buttons';
 import markerBw from '../assets/images/icon/marker-bw.png';
 import markerColor from '../assets/images/icon/marker-color.png';
 
-const BackGround = tw.div`
-  w-screen h-screen flex justify-center items-center flex-col bg-cover
-`;
-
 const RecruitMap = () => {
   return (
-    <BackGround className='flex-col' style={{ backgroundImage: 'url(images/backgrounds/bg3.png)' }}>
-      <UpperPart>
+    <Background img={'bg3'}>
+      <UpperPart className=' flex flex-col'>
         <RecruitTypeIcon></RecruitTypeIcon>
         <RegionButtonsContainer>
           <RegionButton title={'홍대'}></RegionButton>
           <RegionButton title={'강남'}></RegionButton>
           <RegionButton title={'건대'}></RegionButton>
         </RegionButtonsContainer>
-        <MarkerDescriptions className='flex'>
+      </UpperPart>
+      <LowerPart className='flex flex-col px-[10vw]'>
+        <MarkerDescriptions className='flex ml-auto mt-4 mb-8 drop-shadow-md'>
           <Marker src={markerColor} alt='marker-color' description={'모집중'}></Marker>
           <Marker src={markerBw} alt='marker-bw' description={'모집완료'}></Marker>
         </MarkerDescriptions>
-      </UpperPart>
-      <LowerPart className='flex'>
+        <MapContainer></MapContainer>
         <BanggaCharacter />
-        <MapContainer />
-        <InputContainer>지도입니다</InputContainer>
       </LowerPart>
-    </BackGround>
+    </Background>
   );
 };
 
 const Marker = (props) => {
   return (
     <div className='flex flex-col mx-1 h-10'>
-      <img src={props.src} alt={props.alt} className='w-10'></img>
+      <img src={props.src} alt={props.alt} className='w-10 m-auto'></img>
       <div>{props.description}</div>
     </div>
   );
 };
 
 const MapContainer = () => {
-  return (
-    <div>
-      <div>
-        <div></div>
-        <div></div>
-      </div>
-      <div></div>
-    </div>
-  );
+  return <div className='w-[1250px] h-[700px] bg-white opacity-60 rounded-2xl'>지도입니다</div>;
 };
 
 const BanggaCharacter = () => {
@@ -63,11 +51,11 @@ const BanggaCharacter = () => {
 };
 
 const UpperPart = tw.div`
-  flex
+  px-[10vw]
 `;
 
 const LowerPart = tw.div`
-  flex
+  px-[10vw]
 `;
 
 const MarkerDescriptions = tw.div`
@@ -75,11 +63,7 @@ const MarkerDescriptions = tw.div`
 `;
 
 const RegionButtonsContainer = tw.div`
-  flex
-`;
-
-const InputContainer = tw.div`
-  w-[500px] h-[500px] bg-white
+  m-auto
 `;
 
 export default RecruitMap;
