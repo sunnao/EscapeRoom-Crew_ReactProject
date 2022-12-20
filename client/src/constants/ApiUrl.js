47692d4eb2170deb5ea08ee3d1daa6e01ea4ad30
sojinjang <jsojin6119@gmail.com>
2022-12-20 18:10:51 +0900

    :recycle: refactor(env): kakao api key env 파일에서 관리

--- client/src/constants/ApiUrl.js
+++ client/src/constants/ApiUrl.js
@@ -1,3 +1,1 @@
-export const ApiUrl = Object.freeze({
-  KAKAO_MAP: '//dapi.kakao.com/v2/maps/sdk.js?appkey=5f967c751c97ed89fb42b8bd73aa14b4&libraries=services',
-});
+export const ApiUrl = Object.freeze({});
