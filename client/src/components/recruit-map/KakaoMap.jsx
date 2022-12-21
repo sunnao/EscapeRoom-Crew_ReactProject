0317dda70521b435e7143c044ad359b2584ab224
sojinjang <jsojin6119@gmail.com>
2022-12-22 02:16:47 +0900

    :sparkles: feat(map): 마우스 오버 시 카페 이름 보이게 하는 기능, 클릭시 카페 위치를 지도 중심으로 이동 기능 추가

--- client/src/components/recruit-map/KakaoMap.jsx
+++ client/src/components/recruit-map/KakaoMap.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import React, { useState } from 'react';
+import { Map, MapMarker, useMap } from 'react-kakao-maps-sdk';
 import { cafeMockData } from '../../constants/cafeMockData';
 import { regionCoordinate } from '../../constants/regionCoordinate';
 import markerColor from '../../assets/images/icon/marker-color.png';
 
 export default function KakaoMap({ region }) {
+  const MarkerContainer = ({ position, content }) => {
+    const map = useMap();
+    const [isVisible, setIsVisible] = useState(false);
+
+    return (
+      <MapMarker
+        position={position}
+        image={{
+          src: markerColor,
+          size: {
+            width: 35,
+            height: 35,
+          },
+        }}
+        clickable={true}
+        onClick={(marker) => map.panTo(marker.getPosition())}
+        onMouseOver={() => setIsVisible(true)}
+        onMouseOut={() => setIsVisible(false)}>
+        {isVisible && content}
+      </MapMarker>
+    );
+  };
+
   return (
     <Map
       center={regionCoordinate[region]}
@@ -14,17 +37,10 @@ export default function KakaoMap({ region }) {
       }}
       level={5}>
       {cafeMockData.map((cafe) => (
-        <MapMarker
+        <MarkerContainer
           key={`${cafe.title}-${cafe.lat}-${cafe.lng}`}
           position={{ lat: cafe.lat, lng: cafe.lng }}
-          image={{
-            src: markerColor,
-            size: {
-              width: 35,
-              height: 35,
-            },
-          }}
-          title={cafe.title}
+          content={cafe.title}
         />
       ))}
     </Map>
