659761c0d4cfdc8910ed119f75fae2d32e2b476b
sojinjang <jsojin6119@gmail.com>
2022-12-23 02:24:01 +0900

    :lipstick: style(map): 마커 마우스오버시 나타나는 인포 윈도우 커스텀, 모집중 팀 인원 정보 추가
    marker 컴포넌트 안에서 info window 설정해줄 때 자동으로 적용해주는 기본 스타일을 없애지 못하는 이슈 있음

--- client/src/components/recruit-map/KakaoMap.jsx
+++ client/src/components/recruit-map/KakaoMap.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Map, MapMarker, useMap } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, useMap, CustomOverlayMap } from 'react-kakao-maps-sdk';
+
+import { InfoWindow } from './InfoWindow';
 import { regionCoordinate } from '../../constants/regionCoordinate';
 import { ApiUrl } from '../../constants/ApiUrl';
 import * as api from '../../utils/api';
 import markerImg from '../../assets/images/icon/marker.png';
 import clickedMarkerImg from '../../assets/images/icon/marker-clicked.png';
+import { cafeMockData } from '../../constants/cafeMockData';
 
 async function getCafeInfo(region) {
   try {
@@ -16,8 +19,8 @@ async function getCafeInfo(region) {
 }
 
 export default function KakaoMap({ region }) {
-  const BASIC_SIZE = 35;
-  const OVER_SIZE = 40;
+  const BASIC_SIZE = 40;
+  const OVER_SIZE = 42;
   const [target, setTarget] = useState(null);
   const [cafeInfo, setCafeInfo] = useState({});
 
@@ -29,7 +32,7 @@ export default function KakaoMap({ region }) {
     if (!cafeInfo[region]) addRegionCafeData();
   }, [region]);
 
-  const MarkerContainer = ({ cafeId, setTarget, position, content }) => {
+  const MarkerContainer = ({ cafeId, setTarget, position, cafeName, recruitingNum }) => {
     const map = useMap();
     const [isOver, setIsOver] = useState(false);
     let markerIcon = cafeId === target ? clickedMarkerImg : markerImg;
@@ -50,8 +53,19 @@ export default function KakaoMap({ region }) {
           setTimeout(() => setTarget(cafeId));
         }}
         onMouseOver={() => setIsOver(true)}
-        onMouseOut={() => setIsOver(false)}>
-        {isOver && content}
+        onMouseOut={() =>
+          setTimeout(function () {
+            setIsOver(false);
+          })
+        }>
+        {isOver && <InfoWindow cafeName={cafeName} recruitingNum={recruitingNum} />}
+        {/* memo 소진: 아래 주석 -> https://react-kakao-maps-sdk.jaeseokim.dev/docs/sample/overlay/customOverlay2 코드 참고
+        예제랑 다르게 커스텀 오버레이 컴포넌트 위치 이상해짐 22.12.23 */}
+        {/* {isOver && (
+          <CustomOverlayMap position={position} xAnchor={0.3} yAnchor={0.91}>
+            <CustomOverlay cafeName={cafeName} recruitingNum={recruitingNum} />
+          </CustomOverlayMap>
+        )} */}
       </MapMarker>
     );
   };
@@ -71,9 +85,20 @@ export default function KakaoMap({ region }) {
             cafeId={cafe.cafeId}
             setTarget={setTarget}
             position={{ lat: cafe.lat, lng: cafe.lng }}
-            content={cafe.cafeName}
+            cafeName={cafe.cafeName}
+            recruitingNum={cafe.recruitingNum}
           />
         ))}
+      {cafeMockData.map((cafe) => (
+        <MarkerContainer
+          key={cafe.cafeId}
+          cafeId={cafe.cafeId}
+          setTarget={setTarget}
+          position={{ lat: cafe.lat, lng: cafe.lng }}
+          cafeName={cafe.cafeName}
+          recruitingNum={cafe.recruitingNum}
+        />
+      ))}
     </Map>
   );
 }
