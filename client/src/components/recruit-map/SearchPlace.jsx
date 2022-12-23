e0347c448fe0c53e8c84bc7b08f07dd28b5443ec
sojinjang <jsojin6119@gmail.com>
2022-12-24 02:56:21 +0900

    :fire: remove(map): 필요 없어진 키워드로 검색해 마커 찍는 카카오 지도 컴포넌트 삭제

--- client/src/components/recruit-map/SearchPlace.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MapContainer from './MapContainer';
-
-const SearchPlace = ({ region }) => {
-  return <MapContainer searchPlace={region + '역 방탈출 카페'} />;
-};
-
-export default SearchPlace;
