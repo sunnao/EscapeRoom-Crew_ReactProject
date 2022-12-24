5fbc8fd0514355d623ea385c600a9c78dfb7fc85
Donghyun Kim <crtmt97@gmail.com>
2022-12-24 13:10:53 +0900

    ✨feat:로그인/회원가입 관련기능 처리

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,7 +6,7 @@ import MyPage from '../pages/MyPage';
 import MatchingList from '../pages/MatchingList';
 import NotFound from '../pages/NotFound';
 import Register from '../pages/Register';
-import EditUserInfo from '../pages/Mypage/EditUserInfo';
+import MypageEdit from '../pages/MypageEdit';
 import CafeList from '../pages/CafeList';
 import RecruitList from '../pages/recruit-list/RecruitList';
 
@@ -16,11 +16,11 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/recruit-list' element={<RecruitList />} />
-        <Route path='/recruit-map' element={<Home />} />
+        <Route path='/recruit-map' element={<RecruitList />} />
         <Route path='/cafelist' element={<CafeList />} />
         <Route path='/mypage' element={<MyPage />} />
         <Route path='/matching-list' element={<MatchingList />} />
-        <Route path='/mypage/edit' element={<EditUserInfo />} />
+        <Route path='/mypage/edit' element={<MypageEdit />} />
         <Route path='/register' element={<Register />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='*' element={<NotFound />} />
