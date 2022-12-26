7eb2f97dad45f15a0e5f2c74ff9230f537b14750
ULemong <maintain9912@gmail.com>
2022-12-27 00:27:39 +0900

    :pencil2: fix: 변수 삭제

--- client/src/components/mypage/UserProfile.jsx
+++ client/src/components/mypage/UserProfile.jsx
@@ -4,16 +4,14 @@ import ProfileWithHat from '../common/ProfileWithHat';
 import userImg from '../../assets/images/user-profile/지현.jpeg';
 
 const UserProfile = ({ userData }) => {
-  const nickName = userData.nickName;
-  const userIntro = userData.userIntro;
   const navigate = useNavigate();
 
   return (
     <div>
       <ProfileWithHat img={userImg} />
       <div className='pl-[15px]'>
-        <h2 className='font-bold text-3xl'>{nickName}</h2>
-        <div className='font-medium text-xl mb-[3px]'>{userIntro}</div>
+        <h2 className='font-bold text-3xl'>{userData.nickName}</h2>
+        <div className='font-medium text-xl mb-[3px]'>{userData.userIntro}</div>
         <button
           onClick={() => navigate('/mypage/edit')}
           className='font-semibold text-white border-4 bg-amber-500 shadow-lg shadow-gray-500/50 my-[10px] px-[15px] py-[5px] rounded-lg'>
