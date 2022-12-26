cd53d5718f044f42a5fc546b7413e72dc93c8584
ULemong <maintain9912@gmail.com>
2022-12-26 19:32:16 +0900

    :sparkles: feat: 공통 컴포넌트 사용

--- client/src/components/mypage/UserProfile.jsx
+++ client/src/components/mypage/UserProfile.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Profile from '../common/Profile';
+import ProfileWithHat from '../common/ProfileWithHat';
+import userImg from '../../assets/images/user-profile/지현.jpeg';
 
 const UserProfile = ({ userData }) => {
+  const nickName = userData.nickName;
+  const userIntro = userData.userIntro;
   const navigate = useNavigate();
-  console.log('UserProfile', userData);
+
   return (
     <div>
-      <Profile></Profile>
+      <ProfileWithHat img={userImg} />
       <div className='pl-[15px]'>
-        <h2 className='font-bold text-3xl'>{userData.nickName}</h2>
-        <div className='font-medium text-xl mb-[3px]'>{userData.userIntro}</div>
+        <h2 className='font-bold text-3xl'>{nickName}</h2>
+        <div className='font-medium text-xl mb-[3px]'>{userIntro}</div>
         <button
           onClick={() => navigate('/mypage/edit')}
           className='font-semibold text-white border-4 bg-amber-500 shadow-lg shadow-gray-500/50 my-[10px] px-[15px] py-[5px] rounded-lg'>
