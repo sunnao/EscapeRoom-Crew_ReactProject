05629e06291662d37825857e12efa788f49fc977
sojinjang <jsojin6119@gmail.com>
2022-12-28 02:24:44 +0900

    :lipstick: style(map): info window 스타일 설정 삭제

--- client/src/components/recruit-map/InfoWindow.jsx
+++ client/src/components/recruit-map/InfoWindow.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const InfoWindow = ({ cafeName, recruitingNum }) => {
   return (
-    <div id='controlCustomOverlay' className='wrap rounded-2xl bg-purple-200 bg-opacity-60 '>
+    <div id='controlCustomOverlay'>
       <div className='title text-sm font-medium'>{cafeName}</div>
       <div className='recruiting-info text-xs'>방가방가에서 모집중인 팀 {recruitingNum}팀</div>
     </div>
