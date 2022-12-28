0d6a48ca26012ea0ff521c21cd11d9a3611a7f2a
sojinjang <jsojin6119@gmail.com>
2022-12-28 16:46:40 +0900

    :lipstick: style(map): 인포윈도우 스타일 변경

--- client/src/components/recruit-map/InfoWindow.jsx
+++ client/src/components/recruit-map/InfoWindow.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const InfoWindow = ({ cafeName, recruitingNum }) => {
   return (
-    <div id='controlCustomOverlay'>
-      <div className='title text-sm font-medium'>{cafeName}</div>
-      <div className='recruiting-info text-xs'>방가방가에서 모집중인 팀 {recruitingNum}팀</div>
+    <div id='controlCustomOverlay' className='bg-white'>
+      <div className='title text-sm font-semibold'>{cafeName}</div>
+      <div className='recruiting-info text-xs'>방가방가에서 {recruitingNum}팀 모집중</div>
     </div>
   );
 };
