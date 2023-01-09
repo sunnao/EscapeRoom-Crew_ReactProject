489cbe68e6e995bfff12d39af6d8892ac710aa60
sojinjang <jsojin6119@gmail.com>
2023-01-09 23:03:06 +0900

    :recycle: refactor(api): map api url 상수 변수명 변경

--- client/src/mocks/mapHandler.js
+++ client/src/mocks/mapHandler.js
@@ -4,10 +4,13 @@ import { MapMarker } from './data/map/MapMarker';
 import { MapCafeInfo } from './data/map/MapCafeInfo';
 
 export const mapHandler = [
-  rest.get(process.env.REACT_APP_SERVER_URL + ApiUrl.CAFE_INFO + '/:region', async (req, res, ctx) => {
+  rest.get(process.env.REACT_APP_SERVER_URL + ApiUrl.MAP_MATCHING_POSTS + '/:region', async (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(MapMarker[req.params.region]));
   }),
-  rest.get(process.env.REACT_APP_SERVER_URL + ApiUrl.RECRUITING_INFO + '/:cafeId', async (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(MapCafeInfo[req.params.cafeId]));
-  }),
+  rest.get(
+    process.env.REACT_APP_SERVER_URL + ApiUrl.MAP_MATCHING_POST_CAFE_INFO + '/:cafeId',
+    async (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(MapCafeInfo[req.params.cafeId]));
+    },
+  ),
 ];
