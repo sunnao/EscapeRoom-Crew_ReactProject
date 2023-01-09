256a711a79c7b11b2f23b8a5b098da1077fa5977
sojinjang <jsojin6119@gmail.com>
2023-01-09 22:55:05 +0900

    :recycle: refactor(msw): map 관련 handler 이름 변경

--- client/src/mocks/worker.js
+++ client/src/mocks/worker.js
@@ -1,4 +1,4 @@
 import { setupWorker } from 'msw';
-import { handlers } from './handlers';
+import { mapHandler } from './mapHandler';
 
-export const worker = setupWorker(...handlers);
+export const worker = setupWorker(...mapHandler);
