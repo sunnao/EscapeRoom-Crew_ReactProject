1d5e0306d7ecdd4b23290ef4e56cddf14f020d6f
sunnao <osa125125@naver.com>
2023-02-04 00:02:06 +0900

    ✨ feat(MSW): 카페리스트 목데이터 추가 및 cafeHandler 설정 기존 카페리스트 전체DB json변환. 지역별 api요청 시 req 설정

--- client/src/mocks/handlers/cafeHandler.js
+++ client/src/mocks/handlers/cafeHandler.js
@@ -6,4 +6,8 @@ export const cafeHandler = [
   rest.get(process.env.REACT_APP_SERVER_URL + ApiUrl.ALL_CAFE_DATA, async (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(CafeInfoAll));
   }),
+
+  rest.get(process.env.REACT_APP_SERVER_URL + ApiUrl.REGION_CAFE_DATA + '/:region', async (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(CafeInfoAll.filter((obj) => obj.locationDetail === req.params.region)));
+  }),
 ];
